8dff3c44180057f3f59ed23d990e076de9a21207
Madhavi Jayarathne <37529994+MadhiJay@users.noreply.github.com>
2021-10-03 15:45:10 +0530

    Update con.php

--- api/con.php
+++ api/con.php
@@ -3,25 +3,29 @@ define('SERVER','localhost:3306');
 define('USER','user');
 define('PASSWORD','123');
 define('DATABASE','hackintra');
-class DB{
-	function connect(){
+class DB {
+	
+	function connect() {
 		$conn = mysqli_connect(SERVER, USER, PASSWORD,DATABASE);
 		if (!$conn) {
 			die("Connection failed: ".mysqli_connect_error());
 		}
 		return $conn;
-	}	
-	function disconnect($conn){
+	}
+	
+	function disconnect($conn) {
 		mysqli_close($conn);
 	}
-	function set_data($conn,$sql){
+	
+	function set_data($conn,$sql) {
 		if (mysqli_query($conn, $sql)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	function get_data($conn,$sql){
+	
+	function get_data($conn,$sql) {
 		$result = mysqli_query($conn, $sql);
 		return $result;
 	}
